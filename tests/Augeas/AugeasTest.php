deb93f0b372dd7e044e926ad33e79e34de6dac5e
Stefan Cvetkovic <stefanlex90@gmail.com>
2020-02-10 00:22:28 +0100

    Initial commit

---
+++ tests/Augeas/AugeasTest.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+namespace Augeas;
+
+class AugeasTest extends \PHPUnit\Framework\TestCase {
+
+    public function testToBeDeleted()
+    {
+        $this->assertEquals(true, true);
+    }
+}
